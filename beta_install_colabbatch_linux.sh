38ac7cb7d998b94b1f831ad8f75cb8d2267cc258
YoshitakaMo <virgospica93@gmail.com>
2023-01-31 01:01:35 +0900

    ColabFold beta

---
+++ beta_install_colabbatch_linux.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+type wget || { echo "wget command is not installed. Please install it at first using apt or yum." ; exit 1 ; }
+type curl || { echo "curl command is not installed. Please install it at first using apt or yum. " ; exit 1 ; }
+
+CURRENTPATH=`pwd`
+COLABFOLDDIR="${CURRENTPATH}/colabfold_batch"
+
+mkdir -p ${COLABFOLDDIR}
+cd ${COLABFOLDDIR}
+wget -q -P . https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p ${COLABFOLDDIR}/conda
+rm Miniconda3-latest-Linux-x86_64.sh
+. "${COLABFOLDDIR}/conda/etc/profile.d/conda.sh"
+export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
+conda create -p $COLABFOLDDIR/colabfold-conda python=3.8 -y
+conda activate $COLABFOLDDIR/colabfold-conda
+conda update -n base conda -y
+conda install -c conda-forge python=3.8 cudnn==8.2.1.32 cudatoolkit==11.1.1 openmm==7.5.1 pdbfixer -y
+# Download the updater
+wget -qnc https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/update_linux.sh --no-check-certificate
+chmod +x update_linux.sh
+# install alignment tools
+conda install -c conda-forge -c bioconda kalign2=2.04 hhsuite=3.3.0 mmseqs2=14.7e284 -y
+# install ColabFold and Jaxlib
+# colabfold-conda/bin/python3.8 -m pip install "colabfold[alphafold] @ git+https://github.com/sokrypton/ColabFold"
+colabfold-conda/bin/python3.8 -m pip install -q --no-warn-conflicts "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold@beta"
+colabfold-conda/bin/python3.8 -m pip install https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.3.25+cuda11.cudnn82-cp38-cp38-manylinux2014_x86_64.whl
+colabfold-conda/bin/python3.8 -m pip install jax==0.3.25 biopython==1.79
+
+# bin directory to run
+mkdir -p $COLABFOLDDIR/bin
+cd $COLABFOLDDIR/bin
+cat << EOF > colabfold_batch
+#!/bin/sh
+export TF_FORCE_UNIFIED_MEMORY="1"
+export XLA_PYTHON_CLIENT_MEM_FRACTION="4.0"
+export COLABFOLDDIR=$COLABFOLDDIR
+export PATH="\${COLABFOLDDIR}/colabfold-conda/bin:\$PATH"
+\$COLABFOLDDIR/colabfold-conda/bin/colabfold_batch \$@
+EOF
+chmod +x colabfold_batch
+
+# Use 'Agg' for non-GUI backend
+cd ${COLABFOLDDIR}/colabfold-conda/lib/python3.8/site-packages/colabfold
+sed -i -e "s#from matplotlib import pyplot as plt#import matplotlib\nmatplotlib.use('Agg')\nimport matplotlib.pyplot as plt#g" plot.py
+# modify the default params directory
+sed -i -e "s#appdirs.user_cache_dir(__package__ or \"colabfold\")#\"${COLABFOLDDIR}/colabfold\"#g" download.py
+
+# start downloading weights
+cd ${COLABFOLDDIR}
+colabfold-conda/bin/python3.8 -m colabfold.download
+cd ${CURRENTPATH}
+
+echo "Download of alphafold2 weights finished."
+echo "-----------------------------------------"
+echo "Installation of colabfold_batch finished."
+echo "Add ${COLABFOLDDIR}/bin to your environment variable PATH to run 'colabfold_batch'."
+echo "i.e. For Bash, export PATH=\"${COLABFOLDDIR}/bin:\$PATH\""
+echo "For more details, please type 'colabfold_batch --help'."
---
+++ beta_update_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+COLABFOLDDIR=$1
+
+if [ ! -d $COLABFOLDDIR/colabfold-conda ]; then
+    echo "Error! colabfold-conda directory is not present in $COLABFOLDDIR."
+    exit 1
+fi
+
+pushd $COLABFOLDDIR || { echo "${COLABFOLDDIR} is not present." ; exit 1 ; }
+
+# get absolute path of COLABFOLDDIR
+COLABFOLDDIR=$(cd $(dirname colabfold_batch); pwd)
+# activate conda in $COLABFOLDDIR/conda
+. ${COLABFOLDDIR}/conda/etc/profile.d/conda.sh
+export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
+conda activate $COLABFOLDDIR/colabfold-conda
+# reinstall colabfold and alphafold-colabfold
+python3.8 -m pip uninstall -q "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold@beta" -y
+python3.8 -m pip uninstall alphafold-colabfold -y
+python3.8 -m pip install --no-warn-conflicts "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold@beta"
+
+# use 'agg' for non-GUI backend
+pushd ${COLABFOLDDIR}/colabfold-conda/lib/python3.8/site-packages/colabfold
+sed -i -e "s#from matplotlib import pyplot as plt#import matplotlib\nmatplotlib.use('Agg')\nimport matplotlib.pyplot as plt#g" plot.py
+sed -i -e "s#appdirs.user_cache_dir(__package__ or \"colabfold\")#\"${COLABFOLDDIR}/colabfold\"#g" download.py
+popd
+popd
