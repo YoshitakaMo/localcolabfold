76b2216fa16182d986455caa7447cebc38a3b7eb
Milot Mirdita <milot@mirdita.de>
2023-12-29 16:33:21 +0900

    Update localcolabfold to work with ColabFold > 1.5.5

--- install_colabbatch_linux.sh
+++ install_colabbatch_linux.sh
@@ -1,51 +1,49 @@
-#!/bin/bash
+#!/bin/bash -e
 
-type wget || { echo "wget command is not installed. Please install it at first using apt or yum." ; exit 1 ; }
-type curl || { echo "curl command is not installed. Please install it at first using apt or yum. " ; exit 1 ; }
+type wget 2>/dev/null || { echo "wget is not installed. Please install it using apt or yum." ; exit 1 ; }
 
 CURRENTPATH=`pwd`
 COLABFOLDDIR="${CURRENTPATH}/localcolabfold"
 
-mkdir -p ${COLABFOLDDIR}
-cd ${COLABFOLDDIR}
+mkdir -p "${COLABFOLDDIR}"
+cd "${COLABFOLDDIR}"
 wget -q -P . https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-bash ./Mambaforge-Linux-x86_64.sh -b -p ${COLABFOLDDIR}/conda
+bash ./Mambaforge-Linux-x86_64.sh -b -p "${COLABFOLDDIR}/conda"
 rm Mambaforge-Linux-x86_64.sh
-. "${COLABFOLDDIR}/conda/etc/profile.d/conda.sh"
-export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
-conda create -p $COLABFOLDDIR/colabfold-conda python=3.10 -y
-conda activate $COLABFOLDDIR/colabfold-conda
-conda update -n base conda -y
-conda install git
-conda install -c conda-forge python=3.10 cudnn==8.2.1.32 cudatoolkit==11.6.0 openmm==7.7.0 pdbfixer -y
-# Download the updater
-wget -qnc https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/update_linux.sh --no-check-certificate
-chmod +x update_linux.sh
-# install alignment tools
-conda install -c conda-forge -c bioconda kalign2=2.04 hhsuite=3.3.0 mmseqs2=14.7e284 -y
-# install ColabFold and Jaxlib
-# colabfold-conda/bin/python3.10 -m pip install "colabfold[alphafold] @ git+https://github.com/sokrypton/ColabFold"
-colabfold-conda/bin/python3.10 -m pip install --upgrade pip
-colabfold-conda/bin/python3.10 -m pip install --no-warn-conflicts "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold" tensorflow==2.12.0
-colabfold-conda/bin/python3.10 -m pip install https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.3.25+cuda11.cudnn82-cp310-cp310-manylinux2014_x86_64.whl
-colabfold-conda/bin/python3.10 -m pip install jax==0.3.25 chex==0.1.6 biopython==1.79
 
+source "${COLABFOLDDIR}/conda/etc/profile.d/conda.sh"
+export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
+conda update -n base conda -y
+conda create -p "$COLABFOLDDIR/colabfold-conda" -c conda-forge -c bioconda \
+    git python=3.10 openmm==7.7.0 pdbfixer \
+    kalign2=2.04 hhsuite=3.3.0 mmseqs2=15.6f452 -y
+conda activate "$COLABFOLDDIR/colabfold-conda"
+
+# install ColabFold and Jaxlib
+"$COLABFOLDDIR/colabfold-conda/bin/pip" install --no-warn-conflicts \
+    "colabfold[alphafold-without-jax] @ git+https://github.com/sokrypton/ColabFold"
+"$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade "jax[cuda11_pip]" \
+    -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+# Download the updater
+wget -qnc -O "$COLABFOLDDIR/update_linux.sh" \
+    https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/update_linux.sh
+chmod +x "$COLABFOLDDIR/update_linux.sh"
+
+pushd "${COLABFOLDDIR}/colabfold-conda/lib/python3.10/site-packages/colabfold"
 # Use 'Agg' for non-GUI backend
-cd ${COLABFOLDDIR}/colabfold-conda/lib/python3.10/site-packages/colabfold
 sed -i -e "s#from matplotlib import pyplot as plt#import matplotlib\nmatplotlib.use('Agg')\nimport matplotlib.pyplot as plt#g" plot.py
 # modify the default params directory
 sed -i -e "s#appdirs.user_cache_dir(__package__ or \"colabfold\")#\"${COLABFOLDDIR}/colabfold\"#g" download.py
 # remove cache directory
 rm -rf __pycache__
+popd
 
-# start downloading weights
-cd ${COLABFOLDDIR}
-colabfold-conda/bin/python3.10 -m colabfold.download
-cd ${CURRENTPATH}
-
+# Download weights
+"$COLABFOLDDIR/colabfold-conda/bin/python3" -m colabfold.download
 echo "Download of alphafold2 weights finished."
 echo "-----------------------------------------"
-echo "Installation of colabfold_batch finished."
-echo "Add ${COLABFOLDDIR}/colabfold-conda/bin to your environment variable PATH to run 'colabfold_batch'."
-echo "i.e. For Bash, export PATH=\"${COLABFOLDDIR}/colabfold-conda/bin:\$PATH\""
-echo "For more details, please type 'colabfold_batch --help'."
+echo "Installation of ColabFold finished."
+echo "Add ${COLABFOLDDIR}/colabfold-conda/bin to your PATH environment variable to run 'colabfold_batch'."
+echo -e "i.e. for Bash:\n\texport PATH=\"${COLABFOLDDIR}/colabfold-conda/bin:\$PATH\""
+echo "For more details, please run 'colabfold_batch --help'."
