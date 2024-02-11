82a3635c785b262f7b522419b96790877f67df6c
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-02-11 11:39:58 +0900

    use jax==0.4.23 (#212)

--- update_linux.sh
+++ update_linux.sh
@@ -14,7 +14,7 @@ conda activate "$COLABFOLDDIR/colabfold-conda"
 # reinstall colabfold and alphafold-colabfold
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install --no-warn-conflicts --upgrade --force-reinstall \
     "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold"
-"$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade "jax[cuda11_pip]" \
+"$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade "jax[cuda11_pip]==0.4.23" \
     -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install "colabfold[alphafold]"
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade tensorflow
@@ -22,7 +22,6 @@ conda activate "$COLABFOLDDIR/colabfold-conda"
 
 # use 'agg' for non-GUI backend
 cd "${COLABFOLDDIR}/colabfold-conda/lib/python3.10/site-packages/colabfold"
-# Use 'Agg' for non-GUI backend
 sed -i -e "s#from matplotlib import pyplot as plt#import matplotlib\nmatplotlib.use('Agg')\nimport matplotlib.pyplot as plt#g" plot.py
 # modify the default params directory
 sed -i -e "s#appdirs.user_cache_dir(__package__ or \"colabfold\")#\"${COLABFOLDDIR}/colabfold\"#g" download.py
