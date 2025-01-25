2ecd15b49351a852d0f46507a2546103ac5980e9
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-01-26 00:16:55 +0900

    reinstall jax[cuda12] and numpy

--- update_linux.sh
+++ update_linux.sh
@@ -16,7 +16,7 @@ conda activate "$COLABFOLDDIR/colabfold-conda"
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install --no-warn-conflicts --upgrade --force-reinstall \
     "colabfold[alphafold-minus-jax] @ git+https://github.com/sokrypton/ColabFold"
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install "colabfold[alphafold]"
-"$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade "jax[cuda12]"==0.4.35
+"$COLABFOLDDIR/colabfold-conda/bin/pip" install --force-reinstall "jax[cuda12]"==0.4.35 "numpy==1.26.4"
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install --upgrade tensorflow
 "$COLABFOLDDIR/colabfold-conda/bin/pip" install silence_tensorflow
 
